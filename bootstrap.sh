3504844d53581ae93221c64ab2856b600644bfe8
Ankit Khambhati <akhambhati@gmail.com>
2018-01-29 15:21:08 -0800

    Update bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,6 +1,38 @@
 #!/bin/sh
 
-echo "Setting up Hoth Research Systems..."
+### Pretty(er) printing
+fancy_echo() {
+  local fmt="$1"; shift
+
+  # shellcheck disable=SC2059
+  printf "\n$fmt\n" "$@"
+}
+
+fancy_input() {
+  local fmt="$1"; shift
+
+  # shellcheck disable=SC2059
+  printf "\n$fmt " "$@"
+}
+
+### Initial MacOS Prep
+function initialUpdate() {
+    fancy_echo "Initial pre-setup updates..."
+    
+    # Ask for the administrator password upfront
+    sudo -v
+
+    # Keep-alive: update existing `sudo` time stamp until `osxprep.sh` has finished
+    while true; do sudo -n true; sleep 60; kill -0 "$$" || exit; done 2>/dev/null &
+
+    # Install all available updates
+    fancy_echo "Updating OSX.  If this requires a restart, run the script again."
+    sudo softwareupdate -iva
+    
+    # Install XCode    
+    fancy_echo "Installing Xcode Command Line Tools."
+    xcode-select --install
+}
 
 ### Setup Canonical Directory Structure
 function assembleHoth() {
@@ -57,3 +89,9 @@ else
     fi;
 fi;
 unset doIt;
+
+fancy_echo "Setting up Hoth Research Systems..."
+
+git clone https://github.com/akhambhati/Dotfiles.git $HOME/Dotfiles
+
+source .macos
