87d80048cfb976f0c2ca8d3f6a3571e4aa14dc69
Ankit Khambhati <akhambhati@gmail.com>
2018-01-29 21:41:35 -0800

    Fixed path case-sensitivity

--- remote_bootstrap.sh
+++ remote_bootstrap.sh
@@ -4,8 +4,8 @@
 if [ -z "$CMD" ]; then
   echo "No curl or wget available. Aborting."
 else
-  echo "Installing dotfiles"
-  mkdir -p "$HOME/dotfiles" && \
-  eval "$CMD https://github.com/akhambhati/Dotfiles/tarball/master | tar -xzv -C ~/Dotfiles --strip-components=1 --exclude='{.gitignore}'"
+  echo "Installing Dotfiles"
+  mkdir -p "$HOME/Dotfiles" && \
+  eval "$CMD https://github.com/akhambhati/Dotfiles/tarball/master | tar -xzv -C $HOME/Dotfiles --strip-components=1"
   . "$HOME/Dotfiles/bootstrap.sh"
 fi
