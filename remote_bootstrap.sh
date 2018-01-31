cb4725202c7a9c1efd67ecaab4807944e5726dee
Ankit Khambhati <akhambhati@gmail.com>
2018-01-30 16:40:09 -0800

    Update remote_bootstrap.sh

--- remote_bootstrap.sh
+++ remote_bootstrap.sh
@@ -5,7 +5,7 @@ if [ -z "$CMD" ]; then
   echo "No curl or wget available. Aborting."
 else
   echo "Installing Dotfiles"
-  mkdir -p "$HOME/Dotfiles" && \
+  mkdir -p "$HOME/Dotfiles"
   eval "$CMD https://github.com/akhambhati/Dotfiles/tarball/master | tar -xzv -C $HOME/Dotfiles --strip-components=1"
   . "$HOME/Dotfiles/bootstrap.sh"
 fi
