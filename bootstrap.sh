ea1ed2f298035b12d26bd78b2c5f9ed478bae664
Ankit Khambhati <akhambhati@gmail.com>
2019-03-29 09:28:43 -0700

    Fixed bug related brewfile pointer

--- bootstrap.sh
+++ bootstrap.sh
@@ -133,7 +133,7 @@ function setupCantina() {
 
     # Install all our dependencies with bundle (See Brewfile)
     brew tap homebrew/bundle
-    brew bundle $DOTFILES/Brewfile
+    brew bundle --file=$DOTFILES/Brewfile
     
     # Remove outdated versions from cellar
     brew cleanup
